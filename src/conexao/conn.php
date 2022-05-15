32e4290ace375bb3c7ae7d6dfd684002b33ee5aa
Millenapires32 <millena.silva32@etec.sp.gov.br>
2022-05-14 22:57:23 -0300

    commit

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -2,6 +2,15 @@
 
 //Carregar as credenciais do banco de bados 
 $hostname = "sql109.epizy.com";
-$database = "";
-$user ="";
-$passaword = "";
+$database = "epiz_31629778_sysrifa";
+$user ="	epiz_31629778";
+$passaword = "k1xhKt3Yuyy9hI";
+
+try{
+    $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user, $passaword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Conexão com banco de dados '.$database.', foi realizado com sucesso!';
+
+} catch(PDOException $e){
+    echo 'Erro: '.$e->getMessage();
+}
