62d57658647d4875816266c8e9462090da57f820
Millenapires32 <millena.silva32@etec.sp.gov.br>
2022-05-28 16:59:01 -0300

    commit

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -9,7 +9,7 @@ $passaword = "k1xhKt3Yuyy9hI";
 try{
     $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user, $passaword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'Conexão com banco de dados '.$database.', foi realizado com sucesso!';
+    //echo 'Conexão com banco de dados '.$database.', foi realizado com sucesso!';
 
 } catch(PDOException $e){
     echo 'Erro: '.$e->getMessage();
