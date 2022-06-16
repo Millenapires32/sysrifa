1fb5b34bd2a625f860b8b73ccb03a6789d3c4a2f
Millenapires32 <millena.silva32@etec.sp.gov.br>
2022-06-16 19:00:38 -0300

    Integração

--- src/tipo/modelo/list-tipo.php
+++ src/tipo/modelo/list-tipo.php
@@ -18,3 +18,38 @@ $qtdeLinhas = $resultado->rowCount();
 
 //Verificar se existe algum filtro determinado pelo usuario
 $filtro = $requestData['search']['value'];
+if(!empty($filtro)){
+    //Montar a expressão logica em sql para filtrar a nossa tabela 
+    $sql .= " AND (ID LIKE '$filtro%' ";
+    $sql .= " OR NOME LIKE '$filtro%') ";
+}
+
+//Obter o total de registros existentes na tabela do banco de dados de acordo com o filtro
+$resultado = $pdo->query($sql);
+$totalFiltrados = $resultado->rowCount();
+
+//Obter o valor para a ordenação ORDER BY
+$colunaOrdem = $requestData['order'][0]['column'];
+$ordem = $colunas[$colunaOrdem]['data'];
+$direcao = $requestData['order'][0]['dir'];
+
+//Obter os valores para LIMIT
+$inicio = $requestData['start'];
+$tamanho = $requestData['length'];
+
+//Gerando a nossa ordenação na consulta sql 
+$sql .= "ORDER BY $ordem $direcao LIMIT $inicio, $tamanho";
+$resultado = $pdo->query($sql);
+$dados = array();
+while($row = $resultado->fetch(PDO::FETCH_ASSOC)){
+    $dados[] = array_map('utf8_encode', $row);
+}
+
+//Contruir o nosso objeto JSON no padrao DataTables
+$json_data = array(
+        "draw" => intval($requestData['draw']),
+        "recordsTotal" => intval($qtdeLinhas),
+        "recordsFiltered" => intval($totalFiltrados),
+        "data" => $dados
+);
+echo json_encode($json_data);
